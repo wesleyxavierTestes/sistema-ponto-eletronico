fed7b79f23152aff8e268379abf7bcda440fc0f7
Wesley Xavier <wesleybruce@gmail.com>
2020-07-17 20:20:36 -0300

    init spring

---
+++ src/test/java/com/sistemapontoeletronico/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemapontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
